74a54af63494dc436d17d26006f4e9fe4855856e
AkashMourya9522 <127663659+AkashMourya9522@users.noreply.github.com>
2023-11-06 17:22:20 +0530

    Add files via upload

---
+++ To-Do.js
@@ -0,0 +1,23 @@
+let buttonEle = document.querySelector('.add-button');
+let array = [];
+buttonEle.addEventListener('click',()=>{
+    let inputEle = document.querySelector('.input-element');
+    let inputText = inputEle.value;
+    inputEle.value = '';
+    array.push(inputText);
+    display();
+});
+function display(){
+    let displayEle = document.querySelector('.display-element');
+    let arrayLen = array.length;
+    let concatenate = '';
+    console.log(array);
+    for (let i=0;i<arrayLen;i++){
+        concatenate += `<p> class="paragraph-element">${array[i]}</p><button class="remove-button" onclick="remove(${i})" >Remove</button>`;
+    }
+    document.querySelector('.display-element').innerHTML = concatenate;
+}
+function remove(index){
+    array.splice(index,1);
+    display();
+}
